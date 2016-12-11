96fda94d06205a8ed56430c3353cf711dd8471df
rakeshadloori <rakeshadloori@gmail.com>
2016-12-10 21:05:02 -0600

    Template

---
+++ MovieApp/Content/MyScript.js
@@ -0,0 +1,22 @@
+﻿window.onload = function () {
+   
+    //alert("hi");
+}
+
+
+
+$(window).scroll(function () {
+    var scroll = $(document).scrollTop();
+    console.log(scroll);
+    if (scroll > 70) {
+        $(".navbar").css({ "height": "50px" })
+        $(".navbar-collapse").css({ "margin-top": "-50px" })
+        $(".navbar-collapse").css({ "background-color": "#222222" })
+    }
+
+    else if (scroll < 70) {
+        $(".navbar-collapse").css({ "margin-top": "0px" })
+        $(".navbar").css({ "height": "100px" })
+        
+    }
+});
